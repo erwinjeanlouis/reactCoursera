32d116ac81d6a5004c35bcd32ee9d698c90c2162
Erwin Jeanlouis <erwin_jeanlouis@yahoo.com>
2019-05-11 12:52:29 -0400

    Single Page Application Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderComments({comments}) 
 {
@@ -42,21 +43,32 @@ function RenderDish({dish})
 const  DishDetail = (props) => 
 {    
     if (props.dish != null)
-        return(
+    return (
+        <div className="container">
             <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                    <RenderDish dish={props.dish}/>
-                </div>
-                <div  className="col-12 col-md-5 m-1">
-                    <RenderComments comments={props.dish.comments}/>                                             
-                </div>                             
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
             </div>
-        );
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+        </div>
+    );
     else
         return(
             <div></div>
         );
 }
-
 
 export default DishDetail;
